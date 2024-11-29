f4ad4d2f2ab2e6e0c98e6809485375f3d92bff80
kiran-9777 <kiranbalapradhan46@gmail.com>
2024-11-29 11:48:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nvabar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer' , 'Web Designer' , 'Backend Developer' , 'Frontend Developer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
